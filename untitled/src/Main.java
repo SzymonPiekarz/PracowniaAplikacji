55453c5a71680873594bd10e44f8f4764dea061b
Szymon Piekarz <Szymonpiekarz2007@gmail.com>
2026-09-08 12:30:50 +0200

    Dodawanie projektu do repozytorium zdalnego

---
+++ untitled/src/Main.java
@@ -0,0 +1,10 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+void main() {
+    System.out.println("Hello World"); //sout
+    System.out.print("Cokolwiek");
+    int calkowita = 5;
+    double rzeczywista = 1.5;
+    System.out.println("Wartość zmiennej \"całkowita\" wynosi "+calkowita);
+    System.out.println("Wartość zmiennej \"całkowita\" wynosi po podzieleniu na dwa "+calkowita/2);
+    }
