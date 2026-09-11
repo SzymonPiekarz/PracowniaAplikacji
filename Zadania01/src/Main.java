f488fcd787172914ba78904a85748292faba74bf
SzymonPiekarz <test@test.com>
2026-09-11 11:24:48 +0200

    zadanie01 zadanie 4

--- Zadania01/src/Main.java
+++ Zadania01/src/Main.java
@@ -10,5 +10,11 @@ void main() {
     double liczba= 0.66;
 
     System.out.println("Mam na imię "+imie+", mam "+(2026 - wiek)+" lat i będę pisać maturę za "+liczba+" roku.");
-    
+
+    System.out.println("Podaj temperature: ");
+    Scanner sc = new  Scanner(System.in);
+    double temperatura = sc.nextInt();
+    double Fahrenheit = 1.8 * temperatura+32;
+    System.out.println("Stpnie Celcjusza: "+temperatura+", stopnie Farehita: "+Fahrenheit+".");
+
 }
