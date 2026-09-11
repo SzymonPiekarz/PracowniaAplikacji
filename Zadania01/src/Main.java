bd0e8b1a60984d14713b3f131482a5a6f31ec2f8
SzymonPiekarz <test@test.com>
2026-09-11 10:58:27 +0200

    zadanie01 zadanie 3

--- Zadania01/src/Main.java
+++ Zadania01/src/Main.java
@@ -1,5 +1,14 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 void main() {
-    ;
+    System.out.println("Ania");
+    System.out.println("Bartek");
+    System.out.println("Kasia");
+
+    String imie = "Szymon";
+    int wiek = 2007;
+    double liczba= 0.66;
+
+    System.out.println("Mam na imię "+imie+", mam "+(2026 - wiek)+" lat i będę pisać maturę za "+liczba+" roku.");
+    
 }
