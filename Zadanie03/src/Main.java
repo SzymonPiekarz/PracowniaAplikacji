79a926a11be2eb4db77a375675d6d57065ecc012
SzymonPiekarz <SzymonPiekarz>
2026-09-22 11:55:05 +0200

    Zadanie03.2

---
+++ Zadanie03/src/Main.java
@@ -0,0 +1,20 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+void main() {
+    Scanner sc = new Scanner(System.in);
+    //zadanie 1
+    System.out.println("Podaj liczbe całkowitą dodatnią nieparzystą:");
+    int i = sc.nextInt();
+    for( int j = 1;j <= i; j = j+2 ){
+        System.out.println(j+",");
+    }
+    //zadanie 2
+    System.out.println("Podaj liczbe całkowitą dodatnią parzystą:");
+    int a = sc.nextInt();
+    for( int j = 1;j <= a; j = j*2 ){
+        System.out.println(j+",");
+    }
+    //zadanie 3
+    
+}
+
